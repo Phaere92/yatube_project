077234eed7cd8a96062aa6a33546526e7b963905
Evgeniia Pankova <evgeniia.pankova@dualbootpartners.com>
2023-09-28 13:33:31 +0200

    new branch develop

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse('Посты')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = 'Записи сообщества'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
