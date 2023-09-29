73165ba313a7e7812b32a7a507750f68ac0dc08a
Phaere92 <97865343+Phaere92@users.noreply.github.com>
2023-09-29 09:16:13 +0200

    Develop platform for blogers (#1)
    * add urls and views

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Group, Post
+
+
+def index(request):
+    posts = Post.objects.all()[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group)[:10]
+    title = 'Записи сообщества'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/group_list.html', context)
